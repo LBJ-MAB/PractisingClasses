c2c17c4dde8a34b17602899fc4633c96ba5b04eb
KEYFSLTD\Laurie.Bassett-Jones <laurie.bassett-jones@mab.org.uk>
2025-09-26 11:21:59 +0100

    initial commit

---
+++ PractisingClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PractisingClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("26.09.2025");
+        }
+    }
+}
