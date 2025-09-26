c527f20588ce80f176b2595f7b31e5da05b5f330
KEYFSLTD\Laurie.Bassett-Jones <laurie.bassett-jones@mab.org.uk>
2025-09-26 13:53:54 +0100

    class fundamentals such as fields and methods

--- PractisingClasses/Program.cs
+++ PractisingClasses/Program.cs
@@ -2,11 +2,43 @@
 
 namespace PractisingClasses
 {
+    // Car class
+    class Car
+    {
+        public string color = "red";        // color - field
+        public string model;                // model - field - not defined yet
+        public int maxSpeed;                // max speed - field - not defined yet
+        public int year;                    // year - field - not defined yet
+        
+        // fullThrottle - method
+        public void fullThrottle()
+        {
+            Console.WriteLine("This car is going full speed!");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("26.09.2025");
+            // making an instance of Car class called myObj
+            Car myObj = new Car();
+            // setting the max speed of myObj
+            myObj.maxSpeed = 200;
+            
+            // outputting color of myObj to console
+            Console.WriteLine($"The color of myObj is {myObj.color}");
+            // outputting maxSpped of myObj to console
+            Console.WriteLine("The max speed of myObj is {0}", myObj.maxSpeed);
+            
+            // making a new instance of Car class with different features
+            Car citroenC1 = new Car();
+            citroenC1.maxSpeed = 71;
+            citroenC1.year = 2018;
+            citroenC1.model = "C1";
+            
+            // call the fullThrottle method
+            citroenC1.fullThrottle();
+
         }
     }
 }
--- PractisingClasses/obj/Debug/net8.0/PractisingClasses.AssemblyInfo.cs
+++ PractisingClasses/obj/Debug/net8.0/PractisingClasses.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PractisingClasses")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a308234f18f897bde679462288293acfdccb8b80")]
 [assembly: System.Reflection.AssemblyProductAttribute("PractisingClasses")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PractisingClasses")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
